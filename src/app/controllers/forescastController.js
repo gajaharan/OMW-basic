eeb3fa3926886c538409229fcca9ba59f8f092a7
gajaharan <gsatkunanandan@andigital.com>
2016-03-28 23:42:23 +0100

    Controllers

--- src/app/controllers/forescastController.js
+++ src/app/controllers/forescastController.js
@@ -1,17 +1,18 @@
 angular.module('weatherApp')
-.controller('ForecastCtrl', ['$scope','$filter', 'OpenWeatherMap', function($scope,$filter, OpenWeatherMap) {
+.controller('ForecastCtrl', ['$filter', 'OpenWeatherMap', function($filter, OpenWeatherMap) {
+    var vm = this;
     var PROVIDE_LOC = '! Please provide a location';
     var SERVER_ERROR = '! Unable to get weather information. Please try again later.'
-    $scope.message = '';
-    $scope.validData = false;
+    vm.message = '';
+    vm.validData = false;
 
     var iconUrl = 'http://openweathermap.org/img/w/';
     // Get icon url
-    $scope.getIconUrl = function(icon) {
+    getIconUrl = function(icon) {
       return (icon ? iconUrl + icon + '.png' : '');
     };
     // parse the date to only show day name
-    $scope.parseDate = function (date) {
+    parseDate = function (date) {
       if (new Date(date*1000).setHours(0,0,0,0) === new Date().setHours(0,0,0,0)) {
         return "Today"
       }
@@ -21,28 +22,33 @@ angular.module('weatherApp')
     };      
 
     // Get forecast info by location
-    $scope.getForecastByLocation = function() {
-      if (angular.isUndefined($scope.location) || $scope.location.length == 0) {
-        $scope.message = PROVIDE_LOC;
+    getForecastByLocation = function() {
+      if (angular.isUndefined(vm.location) || vm.location.length == 0) {
+        vm.message = PROVIDE_LOC;
         return;
       }
 
-      getWeatherByCityName($scope.location)
+      getWeatherByCityName(vm.location)
     };
 
 
 	var getWeatherByCityName = function(location) {
 	  // Get weather by city name
 	  OpenWeatherMap.getWeatherByCityName(location).then(function (result) {
-	    $scope.forecast = result;
-	    $scope.message = '';
-	    $scope.validData = true;
+	    vm.forecast = result;
+	    vm.message = '';
+	    vm.validData = true;
 	  }, function (result) { // on failure
-	    $scope.message = SERVER_ERROR;
+	    vm.message = SERVER_ERROR;
 
 	  });
 	} 
 
-    // On load show London weather by default
-    getWeatherByCityName('london');
+  // On load show London weather by default
+  getWeatherByCityName('london');  
+
+  vm.getForecastByLocation = getForecastByLocation;
+  vm.getIconUrl = getIconUrl;
+  vm.parseDate = parseDate;
+
   }]);
