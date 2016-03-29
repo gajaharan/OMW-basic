7df9251ee50167339dc2f589493a9acfa875bcd3
gajaharan <gsatkunanandan@andigital.com>
2016-03-29 22:34:44 +0100

    Directives

--- src/app/controllers/forescastController.js
+++ src/app/controllers/forescastController.js
@@ -4,22 +4,7 @@ angular.module('weatherApp')
     var PROVIDE_LOC = '! Please provide a location';
     var SERVER_ERROR = '! Unable to get weather information. Please try again later.'
     vm.message = '';
-    vm.validData = false;
-
-    var iconUrl = 'http://openweathermap.org/img/w/';
-    // Get icon url
-    getIconUrl = function(icon) {
-      return (icon ? iconUrl + icon + '.png' : '');
-    };
-    // parse the date to only show day name
-    parseDate = function (date) {
-      if (new Date(date*1000).setHours(0,0,0,0) === new Date().setHours(0,0,0,0)) {
-        return "Today"
-      }
-      else {
-        return  $filter('date')(new Date(date * 1000), 'EEEE');
-      }
-    };      
+    vm.validData = false;    
 
     // Get forecast info by location
     getForecastByLocation = function() {
@@ -48,7 +33,5 @@ angular.module('weatherApp')
   getWeatherByCityName('london');  
 
   vm.getForecastByLocation = getForecastByLocation;
-  vm.getIconUrl = getIconUrl;
-  vm.parseDate = parseDate;
 
   }]);
